2aab4941c41caffaf3aaa0990a64f57a4a9c5824
Moomen Eltelbany <moomenmohamed4@gmail.com>
2022-07-24 13:59:21 -0300

    Dynamic progress
    Changes the static progress bar with a dynamic one

--- main.js
+++ main.js
@@ -15,3 +15,49 @@ myBtn.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+// Customizing the progress width of Skills Section
+
+let section = document.querySelector(".skills");
+
+let progressSpans = document.querySelectorAll(".skills span");
+
+// Customizing the Stats section to make it more dynamic
+
+let statSection = document.querySelector(".stats");
+
+let statProgress = document.querySelectorAll(".stats .stat-progress");
+
+let started = false;
+
+function startCount(element) {
+    let goal = element.dataset.number;
+
+    let counter = setInterval(() => {
+        element.textContent++;
+        if (element.textContent == goal) {
+            clearInterval(counter);
+        }
+    }, 3000 / goal);
+}
+
+window.onscroll = () => {
+    //Start the progress width of Skills Section
+    if (window.scrollY >= section.offsetTop - 200) {
+        progressSpans.forEach((ele) => {
+            ele.style.width = ele.dataset.progress;
+        });
+    } else {
+        progressSpans.forEach((ele) => {
+            ele.style.width = "0";
+        });
+    }
+
+    //Start the Stats section to make it more dynamic
+    if (window.scrollY >= statSection.offsetTop - 200) {
+        if (!started) {
+            statProgress.forEach((ele) => startCount(ele));
+        }
+        started = true;
+    } else {}
+};
